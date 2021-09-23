c4c2276d640ee6678c84c7589967f67dddb6215f
kathy-ramos <rmsabigail@gmail.com>
2021-09-22 21:45:10 -0700

    matrix added

--- practice4.R
+++ practice4.R
@@ -1,23 +1,27 @@
-# Free Throws
-# You have been supplide data for two additiona in-game statics:
-# * Free Throws
-# * Free Throws Attempt
-# 
-# You need to create three plots that portray the following insights:
-# * Free Trows Attempts per game
-# * Accurance of Free Trhows
-# * Player playing style (2 vs 3 points preference) excluding Free Throws
-# * Each Free Throw is worth 1 Point
-# 
-# The data has been supplied in the form of vectors. You will have to Create the matrices before you proceed with the analysis.
+You have been supplied data for two more additional in-game statistics:
+  * Free Throws
+* Free Throws Attempts
 
-#Seasons
-Seasons <- c("2005","2006","2007","2008","2009","2010","2011","2012","2013","2014")
+You need to create three plots that portray the following insights:
+  * Free Throw Attempts per game
+* Accurancy of Free Throws
+* Player playing style (2 vs 3 points preference) excluding Free Throws
 
-#Players
-Players <- c("KobeBryant","JoeJohnson","LeBronJames","CarmeloAnthony","DwightHoward","ChrisBosh","ChrisPaul","KevinDurant","DerrickRose","DwayneWade")
+- Each Free Throw is worth 1 point
 
-#Free Throws
+The data has been supplied in the form of vectors. You will have to create
+two matrices before you proceed with the analysis.
+
+#Seasons(vector)
+Seasons <- c("2005","2006","2007","2008","2009",
+             "2010","2011","2012","2013","2014")
+
+#Players(vector)
+Players <- c("KobeBryant","JoeJohnson","LeBronJames",
+             "CarmeloAnthony","DwightHoward","ChrisBosh",
+             "ChrisPaul","KevinDurant",             "DerrickRose","DwayneWade")
+
+#FreeThrows(vector)
 KobeBryant_FT <- c(696,667,623,483,439,483,381,525,18,196)
 JoeJohnson_FT <- c(261,235,316,299,220,195,158,132,159,141)
 LeBronJames_FT <- c(601,489,549,594,593,503,387,403,439,375)
@@ -29,13 +33,14 @@ KevinDurant_FT <- c(209,209,391,452,756,594,431,679,703,146)
 DerrickRose_FT <- c(146,146,146,197,259,476,194,0,27,152)
 DwayneWade_FT <- c(629,432,354,590,534,494,235,308,189,284)
 
-#Matrix
-ft.mat <- matrix(c(KobeBryant_FT, JoeJohnson_FT, LeBronJames_FT,
-       CarmeloAnthony_FT, DwightHoward_FT, ChrisBosh_FT,
-       ChrisPaul_FT, KevinDurant_FT, DerrickRose_FT, DwayneWade_FT),
-       ncol=10)
+#FreeThrows(Matrix)
+Player_FT <- rbind(KobeBryant_FT, JoeJohnson_FT, LeBronJames_FT, CarmeloAnthony_FT, DwightHoward_FT, ChrisBosh_FT, ChrisPaul_FT,
+                   KevinDurant_FT, DerrickRose_FT, DwayneWade_FT)
+colnames(Player_FT) <- c(Seasons) 
+rownames(Player_FT) <- c(Players) 
+Player_FT #show Matrix
 
-#Free Throw Attempts
+#Free Throw Attempts (vector)
 KobeBryant_FTA <- c(819,768,742,564,541,583,451,626,21,241)
 JoeJohnson_FTA <- c(330,314,379,362,269,243,186,161,195,176)
 LeBronJames_FTA <- c(814,701,771,762,773,663,502,535,585,528)
@@ -47,87 +52,133 @@ KevinDurant_FTA <- c(256,256,448,524,840,675,501,750,805,171)
 DerrickRose_FTA <- c(205,205,205,250,338,555,239,0,32,187)
 DwayneWade_FTA <- c(803,535,467,771,702,652,297,425,258,370)
 
+#Free Throw Attempts(Matrix)
+Player_FTA <- rbind(KobeBryant_FTA, JoeJohnson_FTA, LeBronJames_FTA, CarmeloAnthony_FTA, DwightHoward_FTA, ChrisBosh_FTA, ChrisPaul_FTA,
+                    KevinDurant_FTA, DerrickRose_FTA, DwayneWade_FTA)
+colnames(Player_FTA) <- c(Seasons)
+rownames(Player_FTA) <- c(Players)
+Player_FTA #show Matrix
+
+
+
+#Games 
+KobeBryant_G <- c(80,77,82,82,73,82,58,78,6,35)
+JoeJohnson_G <- c(82,57,82,79,76,72,60,72,79,80)
+LeBronJames_G <- c(79,78,75,81,76,79,62,76,77,69)
+CarmeloAnthony_G <- c(80,65,77,66,69,77,55,67,77,40)
+DwightHoward_G <- c(82,82,82,79,82,78,54,76,71,41)
+ChrisBosh_G <- c(70,69,67,77,70,77,57,74,79,44)
+ChrisPaul_G <- c(78,64,80,78,45,80,60,70,62,82)
+KevinDurant_G <- c(35,35,80,74,82,78,66,81,81,27)
+DerrickRose_G <- c(40,40,40,81,78,81,39,0,10,51)
+DwayneWade_G <- c(75,51,51,79,77,76,49,69,54,62)
+
 #Matrix
-fta.mat <- matrix(c(KobeBryant_FTA, JoeJohnson_FTA, LeBronJames_FTA,
-         CarmeloAnthony_FTA, DwightHoward_FTA, ChrisBosh_FTA,
-         ChrisPaul_FTA, KevinDurant_FTA, DerrickRose_FTA, DwayneWade_FT),
-       ncol=10)
+Games <- rbind(KobeBryant_G, JoeJohnson_G, LeBronJames_G, CarmeloAnthony_G, DwightHoward_G, ChrisBosh_G, ChrisPaul_G, KevinDurant_G, DerrickRose_G, DwayneWade_G)
+rm(KobeBryant_G, JoeJohnson_G, CarmeloAnthony_G, DwightHoward_G, ChrisBosh_G, LeBronJames_G, ChrisPaul_G, DerrickRose_G, DwayneWade_G, KevinDurant_G)
+colnames(Games) <- Seasons
+rownames(Games) <- Players
 
-# Hit 1 for the player playing style (Points - FreeThrows) / FieldGoals
+#Points
+KobeBryant_PTS <- c(2832,2430,2323,2201,1970,2078,1616,2133,83,782)
+JoeJohnson_PTS <- c(1653,1426,1779,1688,1619,1312,1129,1170,1245,1154)
+LeBronJames_PTS <- c(2478,2132,2250,2304,2258,2111,1683,2036,2089,1743)
+CarmeloAnthony_PTS <- c(2122,1881,1978,1504,1943,1970,1245,1920,2112,966)
+DwightHoward_PTS <- c(1292,1443,1695,1624,1503,1784,1113,1296,1297,646)
+ChrisBosh_PTS <- c(1572,1561,1496,1746,1678,1438,1025,1232,1281,928)
+ChrisPaul_PTS <- c(1258,1104,1684,1781,841,1268,1189,1186,1185,1564)
+KevinDurant_PTS <- c(903,903,1624,1871,2472,2161,1850,2280,2593,686)
+DerrickRose_PTS <- c(597,597,597,1361,1619,2026,852,0,159,904)
+DwayneWade_PTS <- c(2040,1397,1254,2386,2045,1941,1082,1463,1028,1331)
+#Matrix
+Points <- rbind(KobeBryant_PTS, JoeJohnson_PTS, LeBronJames_PTS, CarmeloAnthony_PTS, DwightHoward_PTS, ChrisBosh_PTS, ChrisPaul_PTS, KevinDurant_PTS, DerrickRose_PTS, DwayneWade_PTS)
+rm(KobeBryant_PTS, JoeJohnson_PTS, LeBronJames_PTS, CarmeloAnthony_PTS, DwightHoward_PTS, ChrisBosh_PTS, ChrisPaul_PTS, KevinDurant_PTS, DerrickRose_PTS, DwayneWade_PTS)
+colnames(Points) <- Seasons
+rownames(Points) <- Players
 
-# Hit 2 fill the Blanks
+#Field Goals
+KobeBryant_FG <- c(978,813,775,800,716,740,574,738,31,266)
+JoeJohnson_FG <- c(632,536,647,620,635,514,423,445,462,446)
+LeBronJames_FG <- c(875,772,794,789,768,758,621,765,767,624)
+CarmeloAnthony_FG <- c(756,691,728,535,688,684,441,669,743,358)
+DwightHoward_FG <- c(468,526,583,560,510,619,416,470,473,251)
+ChrisBosh_FG <- c(549,543,507,615,600,524,393,485,492,343)
+ChrisPaul_FG <- c(407,381,630,631,314,430,425,412,406,568)
+KevinDurant_FG <- c(306,306,587,661,794,711,643,731,849,238)
+DerrickRose_FG <- c(208,208,208,574,672,711,302,0,58,338)
+DwayneWade_FG <- c(699,472,439,854,719,692,416,569,415,509)
 
-#Matrix for Free Throws
-#Bind the given vectors to form the matrix
-FreeThrows <- _(KobeBryant_FT, JoeJohnson_FT, LeBronJames_FT, CarmeloAnthony_FT, DwightHoward_FT, ChrisBosh_FT, ChrisPaul_FT, KevinDurant_FT, DerrickRose_FT, DwayneWade_FT)
-#Remove vectors - we don't need them anymore
-_(KobeBryant_FT, JoeJohnson_FT, CarmeloAnthony_FT, DwightHoward_FT, ChrisBosh_FT, LeBronJames_FT, ChrisPaul_FT, DerrickRose_FT, DwayneWade_FT, KevinDurant_FT)
-#Rename the columns
-_(FreeThrows) <- _
-#Rename the rows
-_(FreeThrows) <- _
+#Matrix
+FieldGoals <- rbind(KobeBryant_FG, JoeJohnson_FG, LeBronJames_FG, CarmeloAnthony_FG, DwightHoward_FG, ChrisBosh_FG, ChrisPaul_FG, KevinDurant_FG, DerrickRose_FG, DwayneWade_FG)
+rm(KobeBryant_FG, JoeJohnson_FG, LeBronJames_FG, CarmeloAnthony_FG, DwightHoward_FG, ChrisBosh_FG, ChrisPaul_FG, KevinDurant_FG, DerrickRose_FG, DwayneWade_FG)
+colnames(FieldGoals) <- Seasons
+rownames(FieldGoals) <- Players
 
-#Check the matrix
-FreeThrows
 
-#Matrix for Free Throw Attempts
-#Bind the given vectors to form the matrix
-FreeThrowAttempts <- _(KobeBryant_FTA, JoeJohnson_FTA, LeBronJames_FTA, CarmeloAnthony_FTA, DwightHoward_FTA, ChrisBosh_FTA, ChrisPaul_FTA, KevinDurant_FTA, DerrickRose_FTA, DwayneWade_FTA)
-#Remove vectors - we don't need them anymore
-_(KobeBryant_FTA, JoeJohnson_FTA, CarmeloAnthony_FTA, DwightHoward_FTA, ChrisBosh_FTA, LeBronJames_FTA, ChrisPaul_FTA, DerrickRose_FTA, DwayneWade_FTA, KevinDurant_FTA)
-#Rename the columns
-_(FreeThrowAttempts) <- _
-#Rename the rows
-_(FreeThrowAttempts) <- _
 
-#Check the matrix
-FreeThrowAttempts
 
-#Re-create the plotting function
-myplot <- function(z, who=1:10) {
-  matplot(t(z[who,,drop=F]), type="b", pch=15:18, col=c(1:4,6), main="Basketball Players Analysis")
-  legend("bottomleft", inset=0.01, legend=Players[who], col=c(1:4,6), pch=15:18, horiz=F)
+
+
+
+### Values & Plot 1 ###
+#Notice how Chris Paul gets few attempts per game <- Según la gráfica de nuestra práctica no es Chris Paul sino Joe Jhonson quien tiene pocos intentos de tiro por juego.
+
+
+plot <- function(data, rows){
+  Data <- data[rows,,drop=F]
+  matplot(t(Data), type = "b", pch = 15:18, col = c(1:4,6))
+  legend("bottomleft", legend= Players[rows], inset= 0.01, pch= 15:18, col= c(1:4,6), horiz= F )
 }
+plot(Player_FTA)
+plot(Player_FTA/Games) #Plot1 Free Trows Attempts per game
 
-#Visualize the new matrices
-_(FreeThrows)
-_(FreeThrowAttempts)
 
-#Part 1 - Free Throw Attempts Per Game 
-#(You will need the Games matrix)
-myplot(_/_)
-#Notice how Chris Paul gets few attempts per game
 
-#Part 2 - Free Throw Accuracy
-myplot(_/_)
-#And yet Chris Paul's accuracy is one of the highest
-#Chances are his team would get more points if he had more FTA's
-#Also notice that Dwight Howard's FT Accuracy is extremely poor
-#compared to other players. If you recall, Dwight Howard's
-#Field Goal Accuracy was exceptional:
-myplot(FieldGoals/FieldGoalAttempts)
-#How could this be? Why is there such a drastic difference?
-#We will see just now...
 
-#Part 3 - Player Style Patterns Excluding Free Throws
-myplot((_-_)/_)
-#Because we have excluded free throws, this plot now shows us
-#the true representation of player style change. We can verify
-#that this is the case because all the marks without exception
-#on this plot are between 2 and 3. That is because Field Goals
-#can only be for either 2 points or 3 points.
-#Insights:
-#1. You can see how players' preference for 2 or 3 point shots
-#   changes throughout their career. We can see that almost all
-#   players in this dataset experiment with their style throughout
-#   their careers. Perhaps, the most drastic change in style has
-#   been experienced by Joe Johnson.
-#2. There is one exception. You can see that one player has not
-#   changed his style at all - almost always scoring only 2-pointers.
-#   Who is this mystert player? It's Dwight Howard! 
-#   Now that explains a lot. The reason that Dwight Howard's
-#   Field Goal accuracy is so good is because he almost always
-#   scores 2-pointers only. That means he can be close to the basket
-#   or even in contact with it. Free throws, on the other hand require
-#   the player to stand 15ft (4.57m) away from the hoop. That's 
-#   probably why Dwight Howard's Free Throw Accuracy is poor.
+### Values & Plot 2 ###
+#And yet Chris Paul's accuracy is one of the highest <- Confirmamos observando la gráfica de precisión.
+#Chances are his team would get more points if he had more FTA's <- Asumiendo que su precisión siga intacta, esto podría ser verdadero.
+#Also notice that Dwight Howard's FT Accuracy is extremely poor compared to other players. <- Se puede observar en el gráfico, siendo Dwight Howard el de menor precisión (línea rosa con cuadritos).
+
+
+plot <- function(data, rows){
+  Data <- data[rows,,drop=F]
+  matplot(t(Data), type = "b", pch = 15:18, col = c(1:4,6))
+  legend("bottomleft", legend= Players[rows], inset= 0.01, pch= 15:18, col= c(1:4,6), horiz= F )
+}
+plot(Player_FTA)
+plot(Player_FT/Player_FTA) #Plot2 Accuracy of Free Throws
+
+### Values & Plot 3 ###
+Because we have excluded free throws, this plot now shows us
+the true representation of player style change. We can verify
+that this is the case because all the marks without exception
+on this plot are between 2 and 3. That is because Field Goals
+can only be for either 2 points or 3 points.
+Insights:
+  1. You can see how players' preference for 2 or 3 point shots
+   changes throughout their career. We can see that almost all
+   players in this dataset experiment with their style throughout
+   their careers. Perhaps, the most drastic change in style has
+   been experienced by Joe Johnson.
+2. There is one exception. You can see that one player has not
+   changed his style at all - almost always scoring only 2-pointers.
+   Who is this mystery player? It's Dwight Howard! 
+  Now that explains a lot. The reason that Dwight Howard's
+   Field Goal accuracy is so good is because he almost always
+   scores 2-pointers only. That means he can be close to the basket
+   or even in contact with it. Free throws, on the other hand require
+   the player to stand 15ft (4.57m) away from the hoop. That's 
+probably why Dwight Howard's Free Throw Accuracy is poor.
+Estas observaciones parecen ser correctas acorde a las gráficas que podemos observar a continuación. Según los resultados obtenidos podríamos asumir que, en el caso de Dwight Howard, parece preferir acercarse al aro para anotar en lugar de tirar a distancia.
+'
+
+plot <- function(data, rows){
+        Data <- data[rows,,drop=F]
+        matplot(t(Data), type = "b", pch = 15:18, col = c(1:4,6))
+        legend("bottomleft", legend= Players[rows], inset= 0.01, pch= 15:18, col= c(1:4,6), horiz= F )
+}
+plot(Player_FTA)
+
+plot((Points - Player_FT) / FieldGoals) #Plot3 Player playing style (2 vs 3 points preference) excluding Free Throws
+
