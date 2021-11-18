6802af6f20eff142eac9dfbb8c7a716101fe6ac3
Kathy Ramos <76889253+kathy-ramos@users.noreply.github.com>
2021-11-18 11:21:54 -0800

    more documentation

--- unit-2/practice1.R
+++ unit-2/practice1.R
@@ -21,21 +21,25 @@ ggplot(df, aes(x = Eating.disorders, y = Depression)) + #Asisgment of variables 
 intervals <- 4
 (interval.size <- round(diff(range(df$Year))/intervals))
 
+#year filter
 start.year <- min(df$Year)
 last.year <- max(df$Year)
 df$YearCat = ''
 
-while (start.year < last.year) {
+
+#start cycle
+while (start.year < last.year) { #comparison by year 
   df[(df$Year < (start.year + interval.size)) & (df$YearCat == ''),"YearCat"] <-
     paste(start.year, '-', start.year + interval.size - 1)
-  print(paste(start.year, '-', start.year + interval.size - 1))
+  print(paste(start.year, '-', start.year + interval.size - 1)) #print result
   start.year <- start.year + interval.size
   
 }
 
+#cat dataframe
 cat.df <- melt(df[,c(4,5,6,7,8,9,10,11)], id.vars = "YearCat")
 
-
+#create plot 
 ggplot(cat.df, aes(y = value, x = variable)) + 
   geom_bar(position = "dodge",
            stat = "summary",
@@ -48,7 +52,7 @@ ggplot(cat.df, aes(y = value, x = variable)) +
     "Anxiety.disorders"="AD", "Drug.use.disorders"="DUD",
     "Alcohol.use.disorders"="AUD"
   )) + 
-  theme(axis.line = element_line(size = 3, colour = "grey80"),
+  theme(axis.line = element_line(size = 3, colour = "grey80"),#theme customization
         panel.border = element_rect(linetype = "dashed", fill = NA))
 
 
@@ -56,7 +60,7 @@ ggplot(cat.df, aes(y = value, x = variable)) +
 corr.df <- cor(df[,c(4,5,6,7,8,9,10)])
 (melted_cormat <- melt(corr.df))
 
-
+#create map
 ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
   geom_tile()+
   scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
@@ -64,13 +68,13 @@ ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
                        name="Pearson\nCorrelation") +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, 
                                    size = 12, hjust = 1))
-print(ggheatmap)
+print(ggheatmap) #print map
 
 
 # Depression histogram
 ggplot(df, aes(x=Depression)) +
-  geom_histogram(binwidth=.5, colour="black", fill="white") +
+  geom_histogram(binwidth=.5, colour="black", fill="white") + #histogram style
   geom_vline(aes(xintercept=mean(Depression, na.rm=T)),   # Ignore NA values for mean
-             color="red", linetype="dashed", size=1) +
-  theme(axis.line = element_line(size = 3, colour = "grey80"),
+             color="red", linetype="dashed", size=1) + #lines style
+  theme(axis.line = element_line(size = 3, colour = "grey80"), #theme customization
         panel.border = element_rect(linetype = "dashed", fill = NA))
